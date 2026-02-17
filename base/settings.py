d502ba84630f5366f4a9f4d4c7f08e253d40c0c8
sibigi <adm.sigi@mail.ru>
2026-02-17 11:20:04 +0700

    view_pdf

--- base/settings.py
+++ base/settings.py
@@ -124,3 +124,4 @@ MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
